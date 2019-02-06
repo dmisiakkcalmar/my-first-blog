2df7ae70bfab17a10cf2994a7709557be115dd55
damian <dmisiak@kcalmar.com>
2019-02-06 14:27:25 +0100

    Logowanie

--- blog/settings.py
+++ blog/settings.py
@@ -120,3 +120,9 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 LOGIN_REDIRECT_URL = '/'
+
+
+LOGIN_URL = '/login/'
+#LOGIN_REDIRECT_URL  = 'login_success'
+
+LOGOUT_REDIRECT_URL = '/login/'
